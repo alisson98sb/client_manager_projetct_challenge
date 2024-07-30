01a7a29f5d40cb77e1527af2240378498526ac32
alisson98sb <alisson98sb@gmail.com>
2024-07-30 16:31:14 -0300

    feat: creating entities

---
+++ src/main/java/com/example/gerenciamento_de_clientes/domain/address/Address.java
@@ -0,0 +1,32 @@
+package com.example.gerenciamento_de_clientes.domain.address;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String street;
+    private String addres_number;
+    private String complement;
+    private String city;
+    private String state;
+    private String zip_code;
+}
---
+++ src/main/java/com/example/gerenciamento_de_clientes/domain/client/Client.java
@@ -0,0 +1,35 @@
+package com.example.gerenciamento_de_clientes.domain.client;
+
+import com.example.gerenciamento_de_clientes.domain.address.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "client")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private String cpf_cnpj;
+
+    private String corporate_reason;
+
+    private String representative;
+
+    private Boolean status_is_active;
+
+    @OneToOne
+    @JoinColumn(name = "id") //Duvida: Eu vou utilizar o proprio id da entidade address ou preciso criar o compo id_address para ser apenas chave estrangeira?
+    private Address address;
+}
